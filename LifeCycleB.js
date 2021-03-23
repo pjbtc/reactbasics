ef2adc167a9c25ae9cd83eeff0428b22096c3138
pjbtc <pjbhattachan@gmail.com>
2021-03-24 00:02:30 +0545

    Add files via upload

---
+++ LifeCycleB.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+ class LifecycleB extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             name: "pjbtc"
+        }
+        console.log("LifeCycleB constructor")
+    }
+
+    static getDerivedStateFromProps(props, state){
+        console.log('LifeCycleB getDerivedStateFromProps')
+        return null
+    }
+    componentDidMount(){
+        console.log('LifeCycleB componentDidMount')
+    }
+
+    shouldComponentUpdate(){
+        console.log('LifeCycleB shouldComponentUpdate')
+        return true
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('LifecycleB getSnapshotBeforeUpdate')
+        return null
+
+    }
+    componentDidUpdate(){
+        console.log('LifeCycleB componentDidUpdate')
+    }
+    
+    render() {
+        console.log('LifeCycleB render')
+        return <div>LifeCycleB</div>
+    }
+}
+export default LifecycleB
